3a82fde62818de63a712ac891b16b5531ba82df6
RoxiDron <Roksana0zed@gmail.com>
2025-03-07 22:05:42 +0300

    дубль 2

--- src/Main.java
+++ src/Main.java
@@ -3,81 +3,85 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1.1");
-        int [] weight = new int [3];
-        weight [0] = 1;
-        weight [1] = 2;
-        weight [2] = 3;
-        int oneWeight = weight [0];
-        int twoWeight = weight [1];
-        int threeWeight = weight [2];
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+        int oneWeight = weight[0];
+        int twoWeight = weight[1];
+        int threeWeight = weight[2];
         System.out.println(oneWeight);
         System.out.println(twoWeight);
         System.out.println(threeWeight);
 
         System.out.println("Task 1.2");
-        double [] fractionalNumbers = {1.57, 7.654,9.986};
-        for (int i = 0; i < fractionalNumbers.length; i++){
+        double[] fractionalNumbers = {1.57, 7.654, 9.986};
+        for (int i = 0; i < fractionalNumbers.length; i++) {
             System.out.println(fractionalNumbers[i]);
         }
         System.out.println("Task 1.3");
-        int [] beautifulnumbers = {8, 23, 20, 17};
+        int[] beautifulnumbers = {8, 23, 20, 17};
         for (int i = 0; i < beautifulnumbers.length; i++) {
             System.out.println(beautifulnumbers[i]);
         }
         System.out.println("Task 2");
-        int [] numbers = {1, 2, 3};
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + ",");
+        {
+            int[] numbers = {1, 2, 3,};
+            System.out.println(Arrays.toString(numbers));
         }
-        double [] numbersFractional = {1.57, 7.654,9.986};
-        for (int i = 0; i < numbersFractional.length; i++) {
-            System.out.print(numbersFractional[i] + ",");
+        {
+        double[] numbersFractional = {1.57, 7.654, 9.986,};
+        System.out.println(Arrays.toString(numbersFractional));
         }
-        int [] numbersBeautiful = {8, 23, 20,};
-        for (int i = 0; i < numbersBeautiful.length; i++) {
-            System.out.print(numbersBeautiful[i] + ",");
+        {
+        int[] numbersBeautiful = {8, 23, 20, 17,};
+        System.out.println(Arrays.toString(numbersBeautiful));
         }
-        int [] numbersBea = {17};
-        for (int i = 0; i < numbersBea.length; i++) {
-            System.out.print(numbersBea[i] + "\n");
-        }
+
+
+
         System.out.println("Task 3");
-        int [] numbersBeat = {8, 23, 20, 17};
-        for (int i = numbersBeat.length - 1; i >=0;  i--) {
-            System.out.print(numbersBeat[i] + ",");
+        {
+            int[] numbers = {3, 2, 1,};
+            System.out.println(Arrays.toString(numbers));
         }
-        double [] numbersFr = {1.57, 7.654,9.986};
-        for (int i = numbersFr.length - 1; i>=0;  i--) {
-            System.out.print(numbersFr[i] + ",");
+        {
+            double[] numbersFractional = {9.986,7.654, 1.57,};
+            System.out.println(Arrays.toString(numbersFractional));
         }
-        int [] numbersTwoThree = {2, 3};
-        for (int i = numbersTwoThree.length - 1; i>=0; i--) {
-            System.out.print(numbersTwoThree[i] + ",");
+        {
+            int[] numbersBeautiful = {17, 20, 23, 8,};
+            System.out.println(Arrays.toString(numbersBeautiful));
         }
-        int [] numbersOne = {1};
-        for (int i = numbersOne.length - 1; i>=0;  i--) {
-            System.out.print(numbersOne[i]+ "\n");
-        }
+
         System.out.println("Task 4");
         int[] arr = new int[]{1, 2, 3};
         for (int i = 0; i < arr.length; i ++) {
+            if (arr[i] % 2 != 0) {
                 arr[i] = arr[i] + 1;
 
+            }
         }
-                System.out.println(Arrays.toString(arr) + " ");
+            System.out.println(Arrays.toString(arr) + " ");
+
 
         int[] arrTwo = new int[]{8, 23, 20, 17};
         for (int i = 0; i < arrTwo.length; i ++) {
-            arrTwo[i] = arrTwo[i] + 1;
+            if (arrTwo[i] % 2 != 0) {
+                arrTwo[i] = arrTwo[i] + 1;
 
+            }
         }
         System.out.println(Arrays.toString(arrTwo) + " ");
         double[] arrThree = new double[]{1.57, 7.654, 9.986};
         for (int i = 0; i < arrThree.length; i ++) {
-            arrThree[i] = arrThree[i] + 1;
+            if (arrThree[i] % 2 != 0) {
+                arrThree[i] = arrThree[i] + 1;
 
+            }
         }
-        System.out.println(Arrays.toString(arrThree) + " ");
+            System.out.println(Arrays.toString(arrThree) + " ");
+
 
 
     }
